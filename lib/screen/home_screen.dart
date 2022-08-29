66275cf303e8f4535c55cacd8040f273efa36214
dduR <ym7596@naver.com>
2022-08-30 00:54:32 +0900

    첫화면
    첫화면 저장

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('포켓몬박사 뚜르'),
+      ),
+      body: Center(
+        child: Text('test'),
+      ),
+    );
+  }
+}
