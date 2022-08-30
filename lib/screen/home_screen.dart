675edaf8d6d0a464ed8306720492b34c1f872dac
dduR <ym7596@naver.com>
2022-08-30 23:04:36 +0900

    버튼생성 테스트

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -14,8 +14,16 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('포켓몬박사 뚜르'),
       ),
-      body: Center(
-        child: Text('test'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(onPressed: (){}, child: Text("test1")),
+            ElevatedButton(onPressed: (){}, child: Text("test2")),
+          ],
+        ),
       ),
     );
   }
