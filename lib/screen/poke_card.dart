c655f21a06d3ad523458eb619c8b3845f93b299b
dduR <ym7596@naver.com>
2022-09-25 19:09:25 +0900

    json 읽고 그리드뷰 생성 그리드뷰 이미지와 한글이름만 뜨게 추가 그리드뷰 inkwell로 링크타기 추가가능하게끔 변경
    스크롤바 추가
    
    상세정보창 스크립트 추가

---
+++ lib/screen/poke_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PokeCard extends StatelessWidget {
+  const PokeCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("name"),),
+      body: Container(),
+    );
+  }
+}
--- lib/screen/poke_dex.dart
+++ lib/screen/poke_dex.dart
@@ -1,13 +1,80 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class PokeDex extends StatelessWidget {
+class PokeDex extends StatefulWidget {
   const PokeDex({Key? key}) : super(key: key);
 
+  @override
+  State<PokeDex> createState() => _PokeDexState();
+}
+
+class _PokeDexState extends State<PokeDex> {
+
+  List? _items = [];
+
+
+  Future<void> readJson() async {
+    final String response = await DefaultAssetBundle.of(context).loadString("asset/pokedexsample.json");
+    final data = await json.decode(response);
+    setState(() {
+      _items = data;
+      print(_items![0]["Name"]);
+    });
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    readJson();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Poke Dex'),
+        title: Text("포켓몬 도감"),
+      ),
+      body: Scrollbar(
+        thickness: 8.0,
+        radius: Radius.circular(8.0),
+
+        child: GridView.builder(
+          itemCount: _items!.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: 8.0/10.0,
+            mainAxisSpacing: 10.0,
+            crossAxisSpacing: 10.0,
+          ),
+        itemBuilder: (BuildContext context, int index){
+            return Container(
+              child: Column(
+
+                children: [
+                  InkWell(
+                    onTap: (){
+                      print(_items!.elementAt(index)["Name_ko"]);
+                    },
+                    child:
+                    Image.network(
+                      _items!.elementAt(index)["URL"].toString(),
+                    ),
+                  ),
+
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: Text(_items!.elementAt(index)["Name_ko"],
+                      style: TextStyle(fontSize: 13),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+        },
+
+        ),
       ),
     );
   }
