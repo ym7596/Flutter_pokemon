7dd558db4695e6b1f2e724c7bcc2a0e6bf26a40c
dduR <ym7596@naver.com>
2022-12-20 21:11:34 +0900

    비교 화면 작성중
    컨트롤러 생성

--- lib/screen/compare_screen.dart
+++ lib/screen/compare_screen.dart
@@ -10,27 +10,110 @@ class CompareScreen extends StatefulWidget {
 }
 
 class _CompareScreenState extends State<CompareScreen> {
+
+
+  final hpController = TextEditingController();
+  final BController = TextEditingController();
+  final DController = TextEditingController();
+  final SController = TextEditingController();
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('내구력/스피드 실능'),
 
       ),
-      body:
-        Container(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: TextField(
-                decoration: InputDecoration(
-                    labelText : 'Input'
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  width: 100.0,
+                  child: Text("HP",
+                    style:
+                  TextStyle(fontSize: 20.0,height: 2.0,color: Colors.black),
+                  ),
                 ),
-              ),
+                 SizedBox(
+                  width: 100.0,
+                  child: TextField(
+                    style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
+                  controller: hpController,
+                  ),
+                )
+              ],
             ),
-          ),
-        )
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  width: 100.0,
+                  child: Text("방어",
+                    style:
+                    TextStyle(fontSize: 20.0,height: 2.0,color: Colors.black),
+                  ),
+                ),
+                 SizedBox(
+                  width: 100.0,
+                  child: TextField(
+                    style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
+                  controller: BController,
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  width: 100.0,
+                  child: Text("특방",
+                    style:
+                    TextStyle(fontSize: 20.0,height: 2.0,color: Colors.black),
+                  ),
+                ),
+                 SizedBox(
+                  width: 100.0,
+                  child: TextField(
+                    style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
+                  controller: DController,
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  width: 100.0,
+                  child: Text("스피드",
+                    style:
+                    TextStyle(fontSize: 20.0,height: 2.0,color: Colors.black),
+                  ),
+                ),
+                 SizedBox(
+                  width: 100.0,
+                  child: TextField(
+                    style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
+                  controller: SController,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      )
 
-    );
+
+
+
+        );
+
+
   }
 }
