4d3c94b97e02478dcb1ca69efcad685abc85c28b
dduR <ym7596@naver.com>
2022-12-20 21:38:45 +0900

    feat : 코드 정리

--- lib/screen/compare_screen.dart
+++ lib/screen/compare_screen.dart
@@ -10,110 +10,59 @@ class CompareScreen extends StatefulWidget {
 }
 
 class _CompareScreenState extends State<CompareScreen> {
-
-
   final hpController = TextEditingController();
   final BController = TextEditingController();
   final DController = TextEditingController();
   final SController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('내구력/스피드 실능'),
-
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  width: 100.0,
-                  child: Text("HP",
-                    style:
-                  TextStyle(fontSize: 20.0,height: 2.0,color: Colors.black),
-                  ),
-                ),
-                 SizedBox(
-                  width: 100.0,
-                  child: TextField(
-                    style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
-                  controller: hpController,
-                  ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  width: 100.0,
-                  child: Text("방어",
-                    style:
-                    TextStyle(fontSize: 20.0,height: 2.0,color: Colors.black),
-                  ),
-                ),
-                 SizedBox(
-                  width: 100.0,
-                  child: TextField(
-                    style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
-                  controller: BController,
-                  ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  width: 100.0,
-                  child: Text("특방",
-                    style:
-                    TextStyle(fontSize: 20.0,height: 2.0,color: Colors.black),
-                  ),
-                ),
-                 SizedBox(
-                  width: 100.0,
-                  child: TextField(
-                    style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
-                  controller: DController,
-                  ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  width: 100.0,
-                  child: Text("스피드",
-                    style:
-                    TextStyle(fontSize: 20.0,height: 2.0,color: Colors.black),
-                  ),
-                ),
-                 SizedBox(
-                  width: 100.0,
-                  child: TextField(
-                    style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
-                  controller: SController,
-                  ),
-                )
-              ],
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('내구력/스피드 실능'),
         ),
-      )
-
-
-
-
-        );
-
-
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RowBox(myController: hpController, name: "체력"),
+              RowBox(myController: BController, name: "방어"),
+              RowBox(myController: DController, name: "특방"),
+              RowBox(myController: SController, name: "스피드"),
+            ],
+          ),
+        ));
   }
 }
+
+class RowBox extends StatelessWidget {
+  final TextEditingController? myController;
+  final String? name;
+  const RowBox({Key? key, required this.myController, required this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+         SizedBox(
+          width: 100.0,
+          child: Text(
+            name!,
+            style: TextStyle(
+                fontSize: 20.0, height: 2.0, color: Colors.black),
+          ),
+        ),
+        SizedBox(
+          width: 100.0,
+          child: TextField(
+            style: TextStyle(
+                fontSize: 20.0, height: 2.0, color: Colors.black),
+            controller: myController,
+          ),
+        )
+      ],
+    );
+  }
+}
+
